771f4543aad78f0426bc0a6029817175c741c74b
kimwook747 <hiwook@gmail.com>
2025-07-15 00:29:42 +0900

    PD control

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ CONFIG = {
     'map_filename': 'map.txt',
     'dt': 0.1,
     'robot_radius': 0.3,
-    'robot_max_v': 1.0,
+    'robot_max_v': 0.8,
     'robot_max_w': np.deg2rad(80),
     'colors': {
         'wall': 'black',
@@ -34,11 +34,11 @@ CONFIG = {
     },
     # [수정] 혼합 제어를 위한 PID 파라미터 재설정
     'pid': {
-        'target_distance': 0.5, # 측면 장애물과 유지할 목표 거리
+        'target_distance': 0.7, # 측면 장애물과 유지할 목표 거리
         'Kp': 1.5,              # 비례 게인 (반응 강도)
-        'Kd': 0.5,              # 미분 게인을 0으로 설정하여 진동 방지
-        'path_weight': 1.5,     # 경로 추종 가중치
-        'avoid_weight': 0.7,    # 장애물 회피 가중치
+        'Kd': 0.2,              # 미분 게인을 0으로 설정하여 진동 방지
+        'path_weight': 1.2,     # 경로 추종 가중치
+        'avoid_weight': 0.9,    # 장애물 회피 가중치
     },
     'detour_trigger_dist': 1.0
 }
